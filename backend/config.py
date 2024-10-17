162030dc5ee2f17d8f238ffb91ade297baac8a72
roy wahome <roy.njuguna@student.moringaschool.com>
2024-10-17 08:43:48 +0300

    the deployed site

--- backend/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///art_gallery.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'supersecretkey')
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ArtworkSubmit from './ArtworkSubmit';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Art Gallery Marketplace</h1>
+      <ArtworkSubmit />
+    </div>
+  );
+}
+
+export default App;
