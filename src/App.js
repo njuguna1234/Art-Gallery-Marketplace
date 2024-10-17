fbc13fe9f1d973eaea328a7a390f176a518aafde
roy wahome <roy.njuguna@student.moringaschool.com>
2024-10-17 12:37:20 +0300

    the frist commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import ArtworkSubmit from './ArtworkSubmit';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ArtworkList from './components/ArtworkList';
+import ArtworkForm from './components/ArtworkForm';
+import ReviewForm from './components/ReviewForm';
+import UserProfile from './components/UserProfile';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="container">
-      <h1>Art Gallery Marketplace</h1>
-      <ArtworkSubmit />
-    </div>
+    <Router>
+      <Navbar />
+      <div className="App container">
+        <div className="container-content">
+          <Routes>
+            <Route path="/" element={<ArtworkList />} />
+            <Route path="/artworks/new" element={<ArtworkForm />} />
+            <Route path="/reviews/new" element={<ReviewForm />} />
+            <Route path="/profile" element={<UserProfile />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
